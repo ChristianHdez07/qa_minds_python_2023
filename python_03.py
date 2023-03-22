7768ccd0c54720dafd6048cf3c3aeff4c7f84044
ChristianHdez07 <christianhr@qamindslab.com>
2023-03-22 16:12:22 -0600

    update python_03

--- python_03.py
+++ python_03.py
@@ -1,28 +1,121 @@
-nombre = input("¿Cual es su nombre?")
-print(type(nombre))
+age = 10
+print(age)
+type(age)
+print(type(age))
 
-edad = input("¿Cual es su edad?")
-edad = int(edad)
-print(type(edad))
+age = 45
+age_hex = hex(age)
+age_oct = oct(age)
+age_bin = bin(age)
+print(age_bin)
+print(type(age_bin))
 
-ciudad = input("¿Cual es su ciudad de origen?")
-print(type(ciudad))
+average = 9.1
+print(average)
+print(type(average))
 
-estado_civil = input("¿Es casado?")
-estado_civil = bool(estado_civil)
-print(type(estado_civil))
+name = "Christian Hernandez - Equipo 'Azul' "
+print(name)
+print(type(name))
 
-numeroa= input("¿Digite un numero entre 1-100?")
-numeroa = int(numeroa)
-print(type(numeroa))
+name = str(10.5)
+print(name)
+print(type(name))
 
-numerob = input("¿Digite un numero entre 100-200?")
-numerob = int(numerob)
-print(type(numerob))
+paragraph = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol
+ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo cons
+equat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+print(paragraph)
+print(type(paragraph))
 
-division = numerob/numerob
-print(division)
+graduate = bool(7)
+print(graduate)
+print(type(graduate))
 
-print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado de la división es {division}")
+'''Instrucciones:
+Cree una variable llamada "nombre" y asígnale su nombre como valor (ej. "Juan").
+Cree una variable llamada "edad" y asígnale su edad como valor (ej. 30).
+Cree una variable llamada "altura" y asígnale su altura en metros (ej. 1.75).
+Cree una variable llamada "casado" y asígnale un valor booleano que indique si está casado o no (ej. False).
+Cree una variable llamada "profesión" y asígnale su profesión como valor (ej. "Ingeniero").
+Imprima en pantalla el valor de cada variable.
+Verifique el tipo de dato de cada una de las variables e imprima en pantalla.
+Extra:
+Imprima en pantalla una frase que combine todos los valores de las variables anteriormente creadas (ej. "Mi nombre es Juan, tengo 30 años, mido 1.75 metros, no estoy casado y soy Ingeniero").
+Nota: Para verificar el tipo de dato de una variable, se puede utilizar la función type().
+'''
+
+name = "Juan"
+print(type(name))
+
+age = 30
+print(type(age))
+
+height = 1.75
+print(type(height))
+
+wedded = False
+print(type(wedded))
+
+profesion =  "Ingeniero"
+print(type(profesion))
+
+print(f"Mi nombre es {name}, tengo {age} años, mido {height} metros, {wedded} casado y soy {profesion}")
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+# 1) Parentesis
+# 2) Exponente
+# 3) Division-Multiplicacion
+# 4) Suma-Resta
+
+result_1 = a + b + c + d
+result_2 = a + b + c + d
+print(result_1)
+print(result_2)
+
+'''
+Instrucciones:
+Cree tres variables numéricas, "a", "b", y "c", y asígnele un valor a cada una (ej. a = 10, b = 20, c = 30).
+Realice las siguientes operaciones y asigne el resultado a una nueva variable:
+(a + b) * c
+ a + b * c
+Imprima en pantalla el resultado de cada operación.
+Explique la diferencia entre los dos resultados obtenidos y cómo la prioridad de los operadores influye en ellos.
+'''
+
+a = 10
+b = 20
+c = 30
+result_1 = (a + b) * c
+result_2 = a + b * c
+print(result_1)
+print(result_2)
+
+name = "Alexis"
+last_name = "Hernandez"
+age = 28
+print(f"Mi nombre es {name} {last_name}, mi edad es {age}")
+
+'''
+Instrucciones:
+Cree tres variables: "name" (que almacene su nombre), "age" (que almacene su edad), y "city" (que almacene el nombre de la ciudad en la que vive).
+Utilice formateo de cadenas para imprimir en pantalla la siguiente frase: "Mi nombre es [nombre], tengo [edad] años, y vivo en [ciudad]."
+Utilice formateo de cadenas para crear una frase que incluya el valor de "pi" (3.14) y "e" (2.71). La frase debe decir: "Los valores de pi y e son aproximadamente 3.14 y 2.71, respectivamente."
+Utilice el método de formato de cadenas f-strings para realizar las tareas 2 y 3.
+Experimente con diferentes maneras de dar formato a las cadenas, como agregar tabulaciones y alineación.
+'''
+
+name = "Christian"
+age = 28
+city = "CDMX"
+pi = 3.1416
+e = 2.71
+print(f"Mi nombre es {name}, tengo {age} años, y vivo en {city}.")
+print(f"Los valores de pi y e son aproximadamente {pi} y {e}, respectivamente.")
 
 
