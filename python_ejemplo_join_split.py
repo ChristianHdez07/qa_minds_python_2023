93b16eb68ff3f4ebbd11cca0e0fb1324ec6242b4
ChristianHdez07 <christianhr@qamindslab.com>
2023-03-27 22:26:38 -0600

    class python_ejemplo_join_split.py

---
+++ python_ejemplo_join_split.py
@@ -0,0 +1,7 @@
+paises =  ["México", "Venezuela", "Argentina", "Brasil", "Colombia", "Panama"]
+
+pretty = "|".join(paises)
+print(pretty)
+
+split_result = "México/Venezuela/Argentina/Brasil/Colombia/Panama".split("/")
+print(split_result)
